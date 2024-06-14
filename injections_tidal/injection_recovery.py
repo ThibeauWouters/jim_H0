c01793be5457c9f295ed7eacf0c56f23484eb8b8
Thibeau Wouters <thibeau.wouters@ldas-pcdev12.ligo.caltech.edu>
2024-06-14 03:33:01 -0700

    fixed error run

--- injections_tidal/injection_recovery.py
+++ injections_tidal/injection_recovery.py
@@ -6,7 +6,7 @@ p = psutil.Process()
 p.cpu_affinity([0])
 import os
 os.environ['CUDA_VISIBLE_DEVICES'] = "3"
-os.environ["XLA_PYTHON_CLIENT_MEM_FRACTION"] = "0.10"
+os.environ["XLA_PYTHON_CLIENT_MEM_FRACTION"] = "0.15"
 import numpy as np
 # Regular imports 
 import argparse
