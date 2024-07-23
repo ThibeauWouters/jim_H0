9c1f3fcbf9be2ab5933fd27bdaf37370412490aa
ThibeauWouters <thibeauwouters@gmail.com>
2024-07-23 18:18:42 +0200

    first set of runs with the new events

--- KN/run_rubin.sh
+++ KN/run_rubin.sh
@@ -4,27 +4,34 @@ export MKL_NUM_THREADS=1
 export NUMEXPR_NUM_THREADS=1
 export OMP_NUM_THREADS=1
 
-lightcurve-analysis \
-    --model Bu2022Ye \
-    --interpolation-type tensorflow \
-    --outdir ./outdir/BNS/$INJECTION_NUM \
-    --label injection_Bu2022Ye_$INJECTION_NUM \
-    --prior ./Bu2022Ye.prior \
-    --tmin 0 \
-    --tmax 20 \
-    --dt 0.5 \
-    --error-budget 1 \
-    --nlive 2048 \
-    --Ebv-max 0 \
-    --injection ./outdir/injection_Bu2022Ye.json \
-    --injection-num $INJECTION_NUM \
-    --injection-detection-limit 23.9,25.0,24.7,24.0,23.3,22.1 \
-    --injection-outfile ./outdir/BNS/$INJECTION_NUM/lc.csv \
-    --generation-seed 42 \
-    --remove-nondetections \
-    --filters sdssu,ps1__g,ps1__r,ps1__i,ps1__y,ps1__z \
-    --rubin-ToO-type BNS \
-    --rubin-ToO \
-    --local-only \
-    --svd-path ./svdmodels \
-    # --plot \
+for INJECTION_NUM in {0..20}; do
+    OUTDIR=./outdir_Rubin/BNS/$INJECTION_NUM
+    
+    echo "========== Running injection $INJECTION_NUM =========="
+    mpiexec -np 16 lightcurve-analysis \
+        --model Bu2022Ye \
+        --interpolation-type tensorflow \
+        --outdir $OUTDIR \
+        --label injection_Bu2022Ye_$INJECTION_NUM \
+        --prior ./Bu2022Ye.prior \
+        --tmin 0 \
+        --tmax 20 \
+        --dt 0.5 \
+        --error-budget 1 \
+        --nlive 2048 \
+        --Ebv-max 0 \
+        --generation-seed 43 \
+        --injection ./outdir_Rubin/injection_Bu2022Ye.json \
+        --injection-num $INJECTION_NUM \
+        --injection-outfile ./outdir_Rubin/BNS/$INJECTION_NUM/lc.csv \
+        --filters sdssu,ps1__g,ps1__r,ps1__i,ps1__y,ps1__z \
+        --rubin-ToO-type BNS \
+        --rubin-ToO \
+        --svd-path ./svdmodels \
+        --plot \
+        --injection-detection-limit 23.9,25.0,24.7,24.0,23.3,22.1 \
+        --local-only \
+        # --ensure-detections
+        # --remove-nondetections \
+        # --injection-detection-limit None \ # for testing the setup, but this is not realistic
+done
--- KN/run_ztf.sh
+++ KN/run_ztf.sh
@@ -4,12 +4,14 @@ export MKL_NUM_THREADS=1
 export NUMEXPR_NUM_THREADS=1
 export OMP_NUM_THREADS=1
 
-for INJECTION_NUM in {2..29}; do
+for INJECTION_NUM in {0..1}; do
+    OUTDIR=./outdir_ZTF/BNS/$INJECTION_NUM
+    
     echo "========== Running injection $INJECTION_NUM =========="
-    mpiexec -np 16 lightcurve-analysis \
+    mpiexec -np 1 lightcurve-analysis \
         --model Bu2022Ye \
         --interpolation-type tensorflow \
-        --outdir ./outdir/BNS/$INJECTION_NUM \
+        --outdir ./outdir_ZTF/BNS/$INJECTION_NUM \
         --label injection_Bu2022Ye_$INJECTION_NUM \
         --prior ./Bu2022Ye.prior \
         --tmin 0 \
@@ -18,17 +20,19 @@ for INJECTION_NUM in {2..29}; do
         --error-budget 1 \
         --nlive 2048 \
         --Ebv-max 0 \
-        --generation-seed 42 \
-        --injection ./outdir/injection_Bu2022Ye.json \
+        --generation-seed 43 \
+        --injection ./outdir_ZTF/injection_Bu2022Ye.json \
         --injection-num $INJECTION_NUM \
-        --injection-outfile ./outdir/BNS/$INJECTION_NUM/lc.csv \
+        --injection-outfile ./outdir_ZTF/BNS/$INJECTION_NUM/lc.csv \
         --filters ztfg,ztfr,ztfi \
         --ztf-sampling \
         --ztf-ToO 300 \
         --local-only \
         --svd-path ./svdmodels \
         --plot \
-        # --ztf-uncertainties \
+        --ztf-uncertainties \
+        --injection-detection-limit 21.7,21.4,20.9 \
+        --ensure-detections
         # --remove-nondetections \
-        # --injection-detection-limit None \ # TODO: for testing, remove later!
+        # --injection-detection-limit None \ # for testing the setup, but this is not realistic
 done
